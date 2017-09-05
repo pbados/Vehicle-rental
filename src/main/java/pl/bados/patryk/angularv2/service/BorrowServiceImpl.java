72748d5dd9a0955a20531fc05183229c988b6353
Patryk <pbados@billennium.pl>
2017-09-05 12:49:42 +0200

    wrzucenie zmiennych autowired do jednego konstruktora oraz wrzucenie zmiennych w addBorrow do konstruktora argumentowego

--- src/main/java/pl/bados/patryk/angularv2/service/BorrowServiceImpl.java
+++ src/main/java/pl/bados/patryk/angularv2/service/BorrowServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.bados.patryk.angularv2.assembler.BorrowAssembler;
 import pl.bados.patryk.angularv2.dto.BorrowDto;
-import pl.bados.patryk.angularv2.model.Borrow;
 import pl.bados.patryk.angularv2.repository.*;
 
 import java.time.LocalDate;
@@ -13,24 +12,19 @@ import java.time.LocalDate;
 public class BorrowServiceImpl implements BorrowService {
 
     private BorrowRepository borrowRepository;
+    private BorrowAssembler borrowAssembler;
 
     @Autowired
-    BorrowAssembler borrowAssembler;
-
-    @Autowired
-    public BorrowServiceImpl(BorrowRepository borrowRepository){
+    public BorrowServiceImpl(BorrowRepository borrowRepository, BorrowAssembler borrowAssembler){
         this.borrowRepository = borrowRepository;
+        this.borrowAssembler = borrowAssembler;
     }
 
     @Override
     public BorrowDto addBorrow(Long vehicleId, LocalDate date, Long borrowerId) {
-        BorrowDto borrowDto = new BorrowDto();
-        borrowDto.setVehicleId(vehicleId);
-        borrowDto.setLd(date);
-        borrowDto.setBorrowerId(borrowerId);
+        BorrowDto borrowDto = new BorrowDto(date, vehicleId, borrowerId);
 
         return borrowDto;
-
     }
 
     @Override
