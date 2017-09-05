64532138553446a3299a4e299f5812d9d71a8198
Patryk <pbados@billennium.pl>
2017-09-05 10:46:45 +0200

    dodanie borrowerDto

--- src/main/java/pl/bados/patryk/angularv2/dto/BorrowerDto.java
+++ src/main/java/pl/bados/patryk/angularv2/dto/BorrowerDto.java
@@ -1,4 +1,30 @@
 package pl.bados.patryk.angularv2.dto;
 
 public class BorrowerDto {
+
+    private String borrowerName;
+
+    @Override
+    public String toString() {
+        return "BorrowerDto{" +
+                "borrowerName='" + borrowerName + '\'' +
+                '}';
+    }
+
+    public BorrowerDto() {
+    }
+
+    public BorrowerDto(String borrowerName) {
+
+        this.borrowerName = borrowerName;
+    }
+
+    public String getBorrowerName() {
+
+        return borrowerName;
+    }
+
+    public void setBorrowerName(String borrowerName) {
+        this.borrowerName = borrowerName;
+    }
 }
