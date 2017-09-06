b795f90c74d4d3e28142d402e6af5e3f170635fb
Patryk <pbados@billennium.pl>
2017-09-06 15:57:10 +0200

    dodanie pierwszej wersji servisu Car

--- src/main/java/pl/bados/patryk/angularv2/service/CarService.java
+++ src/main/java/pl/bados/patryk/angularv2/service/CarService.java
@@ -1,4 +1,16 @@
 package pl.bados.patryk.angularv2.service;
 
+import pl.bados.patryk.angularv2.dto.CarDto;
+import pl.bados.patryk.angularv2.dto.EditCarDto;
+import pl.bados.patryk.angularv2.model.Color;
+
+import java.time.LocalDate;
+
 public interface CarService {
+
+    CarDto addCar(String vehicleName, Color color, LocalDate productionDate, Long producer);
+    void saveCar(CarDto carDto);
+    void saveEditedCar(EditCarDto editCarDto);
+    EditCarDto editCar(Long vehicleId, String vehicleName, Color color, LocalDate productionDate, Long producer);
+
 }
